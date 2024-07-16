cbd86177cc8c26720e67bae3499f41e2fbbf8076
shafeeq8904 <shafeeq8904@gmail.com>
2024-07-16 15:37:35 +0530

    money.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,11 @@
 import {cart} from '../data/cart.js'
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHtml = ''
 
 
-cart.forEach((cartItem)=>{
+cart.forEach((cartItem)=>{   
 
     const productId = cartItem.productId;
     let matchingProduct;
@@ -32,7 +33,7 @@ cart.forEach((cartItem)=>{
                   ${matchingProduct.name }
                 </div>
                 <div class="product-price">
-                  ${matchingProduct.priceCents /100}
+                  $${(matchingProduct.priceCents /100).toFixed(2)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -49,50 +50,44 @@ cart.forEach((cartItem)=>{
 
               <div class="delivery-options">
                 <div class="delivery-options-title">
-                  Choose a delivery option:
+                    Choose a delivery option:
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
+                    <input type="radio" checked class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+                    <div>
                     <div class="delivery-option-date">
-                      Tuesday, June 21
+                        Tuesday, June 21
                     </div>
                     <div class="delivery-option-price">
-                      FREE Shipping
+                        FREE Shipping
                     </div>
-                  </div>
+                    </div>
                 </div>
                 <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
+                    <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+                    <div>
                     <div class="delivery-option-date">
-                      Wednesday, June 15
+                        Wednesday, June 15
                     </div>
                     <div class="delivery-option-price">
-                      $4.99 - Shipping
+                        $4.99 - Shipping
                     </div>
-                  </div>
+                    </div>
                 </div>
                 <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
+                    <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+                    <div>
                     <div class="delivery-option-date">
-                      Monday, June 13
+                        Monday, June 13
                     </div>
                     <div class="delivery-option-price">
-                      $9.99 - Shipping
+                        $9.99 - Shipping
                     </div>
-                  </div>
+                    </div>
                 </div>
-              </div>
+                </div>
             </div>
-          </div>  
+            </div> 
     
     `
 })
