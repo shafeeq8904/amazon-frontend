2b873d76f9ad4d3fd96bebe8c81ff6d8b3b3d258
shafeeq8904 <shafeeq8904@gmail.com>
2024-07-25 22:20:34 +0530

    fetch api products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,9 +1,11 @@
 import {cart, addToCart,
     calculateCartQuantity} from '../data/cart.js';
-import { products  } from '../data/products.js'
+import { products ,loadProducts } from '../data/products.js'
 import { formatCurrency } from './utils/money.js'
 
+loadProducts(renderProductGrid);
 
+function renderProductGrid(){
 
 let productHtml=''
 
@@ -94,3 +96,5 @@ document.querySelectorAll('.js-add-to-cart')
             updateCartQuantity()
         })
     })
+
+}
