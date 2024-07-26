88788941526bd371c7cc6f9c686d615545a449e4
shafeeq8904 <shafeeq8904@gmail.com>
2024-07-26 13:43:11 +0530

    promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,37 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
+/*
 loadProducts(()=>{
     renderOrderSummary();
     renderPaymentSummary();
-})
+})*/
+
+//USING PROMISES !!!!
+
+new Promise((resolve) => {
+    loadProducts(()=>{
+        resolve();
+    })
+    }).then(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+});
+
+
+/*
+new promise((resolve)=>{
+    loadProducts(()=>{
+        resolve();
+        }
+    });
+}).then(()=>{
+    return new promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    })
+}).then(()=>{
+    render.....
+    })
+*/ 
